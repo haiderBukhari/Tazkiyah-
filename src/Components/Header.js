ac70fd470ee3268df7cc343b7733e88d9bc4b9c5
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-10-18 09:26:33 +0500

    redirects added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ export const Header = () => {
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
-                <div class={`${show?'hidden': ''} w-full md:block md:w-auto`} id="navbar-default">
+                <div class={`${show?'': 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
                     <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         {/* <li>
                             <Link to="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
