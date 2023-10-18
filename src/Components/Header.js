5ea10586f9265dad7dbd0186e681b23089bba7ef
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-10-18 09:18:21 +0500

    Header Link Modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export const Header = () => {
     return (
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="https://flowbite.com/" class="flex items-center">
+                <Link to="/" class="flex items-center">
                     <img src={Logo} class="h-20 mr-3" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tazkiyah</span>
                 </Link>
